7ae286de0056f199ad38dcb02c75009cb7ca6365
A Moteesh <amoteesh@As-MacBook-Air.local>
2026-07-26 12:59:41 +0530

    configure Catalyst Data Store and AppSail deployment

--- app.py
+++ app.py
@@ -847,5 +847,5 @@ init_db()
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("X_ZOHO_CATALYST_LISTEN_PORT", os.environ.get("PORT", "5000")))
+    port = int(os.environ.get("PORT", os.environ.get("X_ZOHO_CATALYST_LISTEN_PORT", "5000")))
     app.run(host="0.0.0.0", port=port, debug=os.environ.get("FLASK_DEBUG") == "1")
